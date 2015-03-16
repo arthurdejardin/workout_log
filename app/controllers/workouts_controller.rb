273ea0f3611ae7682c69b9d077d49654c38fa131
Arthur Dejardin <arthur.dejardin@gmail.com>
2015-03-16 13:43:26 +0000

    add boostrap, simple_form, haml and add workout/views

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,12 +1,24 @@
 class WorkoutsController < ApplicationController
 
+	before_action :find_workout, only: [:show, :edit, :edit, :destroy]
+
 	def index
 	end
 
+	def show
+	end
+
 	def new
+		@workout = Workout.new
 	end
 
 	def create
+		@workout = Workout.new(workout_params)
+		if @workout.save
+			redirect_to @workout
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -18,6 +30,14 @@ class WorkoutsController < ApplicationController
 	def destroy
 	end
 	
+	private
 
+	def find_workout
+		@workout = Workout.find(params[:id])
+	end
+
+	def workout_params
+		params.require(:workout).permit(:date, :mood, :length, :workout)
+	end
 
 end
